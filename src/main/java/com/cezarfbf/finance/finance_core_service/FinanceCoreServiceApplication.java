f1052b451c57436d666c930d079b4949d4799b1c
Cezar Filho <35531208+cezarfbf@users.noreply.github.com>
2026-01-17 19:51:46 +0000

    chore: initial Spring Boot project

---
+++ src/main/java/com/cezarfbf/finance/finance_core_service/FinanceCoreServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cezarfbf.finance.finance_core_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceCoreServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceCoreServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cezarfbf/finance/finance_core_service/FinanceCoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cezarfbf.finance.finance_core_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceCoreServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
